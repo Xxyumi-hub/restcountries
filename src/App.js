5a65401ea991563281d2c8e372b981b8b7366351
Franco Ortega <francopdx@gmail.com>
2022-12-05 17:56:11 -0800

    revert refactor

--- src/App.js
+++ src/App.js
@@ -32,51 +32,31 @@ function App() {
       )
     : singleCountry;
 
-  console.log({ filteredCountries });
-
-  // const countriesToDisplay = filteredCountries.map((filteredCountry, i) => {
-  //   if (filteredCountries.length > 1) {
-  //     return (
-  //       <CountryName
-  //         key={i}
-  //         filteredCountry={filteredCountry}
-  //         setSingleCountry={setSingleCountry}
-  //       />
-  //     );
-  //   } else {
-  //     // Narrowed down to a single country
-  //     return <Country key={i} filteredCountry={filteredCountry} />;
-  //   }
-  // });
-
-  const countriesToDisplay = !searchTerm
-    ? null
-    : filteredCountries.length > 10
-    ? 'Too many results'
-    : filteredCountries.length > 1
-    ? filteredCountries.map((filteredCountry, i) => (
+  const countriesToDisplay = filteredCountries.map((filteredCountry, i) => {
+    if (filteredCountries.length > 1) {
+      return (
         <CountryName
           key={i}
           filteredCountry={filteredCountry}
           setSingleCountry={setSingleCountry}
         />
-      ))
-    : // Narrowed down to a single country
-      filteredCountries.map((filteredCountry, i) => (
-        <Country key={i} filteredCountry={filteredCountry} />
-      ));
+      );
+    } else {
+      // Narrowed down to a single country
+      return <Country key={i} filteredCountry={filteredCountry} />;
+    }
+  });
 
   return (
     <div>
       <label>find countries</label>
       <input onChange={(e) => setSearchTerm(e.target.value)}></input>
       <ul>
-        {countriesToDisplay}
-        {/* {searchTerm.length === 0
+        {searchTerm.length === 0
           ? null
           : filteredCountries.length > 10
           ? 'Too many results'
-          : countriesToDisplay} */}
+          : countriesToDisplay}
       </ul>
     </div>
   );
